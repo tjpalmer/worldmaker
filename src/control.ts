bede794b4ec5520c9535de7dc2a28ff1f6003d2c
Tom <tjpalmer@tjpalmer.com>
2016-09-17 07:38:59 -0700

    Zoom (no detail)

--- src/control.ts
+++ src/control.ts
@@ -12,9 +12,14 @@ export class Control {
     canvas.addEventListener('mousedown', event => this.mouseDown(event));
     window.addEventListener('mousemove', event => this.mouseMove(event));
     window.addEventListener('mouseup', event => this.mouseUp(event));
+    canvas.addEventListener('wheel', event => this.wheel(event));
   }
 
   active: boolean;
+
+  altitude() {
+    return this.radius - this.surface();
+  }
 
   last: Vector2;
 
@@ -28,6 +33,10 @@ export class Control {
     let delta = new Vector2(event.screenX, event.screenY).sub(this.last);
     let size = Math.min(window.innerWidth, window.innerHeight);
     delta.divideScalar(size).multiplyScalar(Math.PI);
+    // Slow down rotation when zoomed in.
+    let deltaScale = new Vector2(1, 1).multiplyScalar(this.altitude() / 3);
+    deltaScale.y = Math.min(1.0, deltaScale.y);
+    delta.multiply(deltaScale);
     // let lonLat = new Vector2().copy(<Vector2><any>this.cameraStartLlh);
     // lonLat.add(delta);
     // Constrain y rotation to poles.
@@ -43,15 +52,10 @@ export class Control {
     let axis = new Vector3(position.x, 0, position.z);
     axis.applyAxisAngle(new Vector3(0, 1, 0), Math.PI * 0.5);
     position.applyAxisAngle(axis, -delta.y);
-    // Make sure we don't lose altitude over time.
-    position.normalize().multiplyScalar(this.radius);
-    // console.log('position', this.stage.camera.position);
-    this.stage.camera.lookAt(new Vector3());
-    this.stage.camera.updateMatrix();
-    this.stage.camera.updateProjectionMatrix();
-    this.stage.render();
     // And remember where we were again.
     this.last.set(event.screenX, event.screenY);
+    // Done.
+    this.update();
   }
 
   mouseUp(event: MouseEvent) {
@@ -61,5 +65,34 @@ export class Control {
   radius: number;
 
   stage: Stage;
+
+  surface() {
+    // TODO Work off elevation at current point.
+    // TODO Why does below 1.1 go inside the sphere?
+    return 1.1;
+  }
+
+  update() {
+    let position = this.stage.camera.position;
+    // Make sure we don't change altitude over time.
+    position.normalize().multiplyScalar(this.radius);
+    // console.log('position', this.stage.camera.position);
+    this.stage.camera.lookAt(new Vector3());
+    this.stage.camera.updateMatrix();
+    this.stage.camera.updateProjectionMatrix();
+    this.stage.render();
+  }
+
+  wheel(event: WheelEvent) {
+    // My browser gives -100 for up and 100 for down.
+    let amount = event.deltaY / 100;
+    let surface = this.surface();
+    let altitude = this.radius - surface;
+    altitude *= 1.0 + amount * 1e-1;
+    this.radius = altitude + surface;
+    console.log(this.radius);
+    // TODO How to avoid noise over time?
+    this.update();
+  }
 
 }
