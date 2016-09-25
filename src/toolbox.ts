4f3ce5d98f12a7207fbdac3dc99a4e7e8b2eb5bf
Tom <tjpalmer@tjpalmer.com>
2016-09-25 07:00:59 -0700

    Tweaks and shuffle

--- src/toolbox.ts
+++ src/toolbox.ts
@@ -1,15 +1,16 @@
 export class Toolbox {
 
   constructor(body: HTMLElement) {
-    let toolbox = body.querySelector('.tools');
+    let toolbox = body.querySelector('.toolbox');
     for (let any of <any>toolbox.querySelectorAll('input')) {
       let input: HTMLInputElement = any;
-      input.addEventListener('change', () => {
+      input.addEventListener('click', () => {
         for (let any of <any>toolbox.querySelectorAll('label')) {
           let other: HTMLInputElement = any;
           other.classList.remove('selected');
         }
-        (<HTMLElement>input.closest('label')).classList.add('selected');
+        let selected = toolbox.querySelector('input:checked');
+        (<HTMLElement>selected.closest('label')).classList.add('selected');
       });
     }
   }
